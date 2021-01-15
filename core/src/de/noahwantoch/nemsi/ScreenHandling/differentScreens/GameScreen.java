07bbbc6c68f10ce47940bd17dda82cf17a8fca68
souvinix <noahg0nz011@gmail.com>
2021-01-15 14:26:12 +0100

    Auto-commited

--- core/src/de/noahwantoch/nemsi/ScreenHandling/differentScreens/GameScreen.java
+++ core/src/de/noahwantoch/nemsi/ScreenHandling/differentScreens/GameScreen.java
@@ -28,7 +28,7 @@ public class GameScreen extends AbstractScreen {
         cardGame = new CardGame();
         worldGame = new WorldGame();
 
-        enemy = new Enemy();
+        enemy = new Enemy(); //Später: worldgame.getCurrentEnemy() oder so
         player = Game.getPlayer();
 
         cardGame.startGame(player, enemy);
