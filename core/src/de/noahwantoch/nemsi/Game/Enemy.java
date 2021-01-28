de55f15dbea1cb44355fc7276d804408ff27fa64
souvinix <noahg0nz011@gmail.com>
2021-01-28 03:20:50 +0100

    Auto-commited

--- core/src/de/noahwantoch/nemsi/Game/Enemy.java
+++ core/src/de/noahwantoch/nemsi/Game/Enemy.java
@@ -3,7 +3,6 @@ package de.noahwantoch.nemsi.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import de.noahwantoch.nemsi.EffectModule;
@@ -128,7 +127,7 @@ public class Enemy extends PlayingPossibilities{
 
     @Override
     public void summonDirectly(int handcardIndex) {
-        if(handcardIndex < handcards.size() - 1){
+        if(handcardIndex < handcards.size()){
             Card card = handcards.get(handcardIndex);
             card.toOpen();
             cardCounter += 1;
@@ -141,7 +140,10 @@ public class Enemy extends PlayingPossibilities{
             }
 
             saveFieldcards();
-        }else Gdx.app.debug("Gegner", "Gescheiterte Beschwörung: " + handcardIndex);
+        }else {
+            Gdx.app.debug("Gegner", "Gescheiterte Beschwörung: " + handcardIndex + ", name: " + handcards.get(handcardIndex).getName() + ", Tribute: " + handcards.get(handcardIndex).getTribute().getNeededCards() + " von dem Element: " + handcards.get(handcardIndex).getTribute().getNeededElement());
+            cardCounter += 1;
+        }
     }
 
     @Override
@@ -211,74 +213,76 @@ public class Enemy extends PlayingPossibilities{
     /** FUNCTIONS FOR ARTIFICIAL INTELLIGENCE */
 
     private void summonBestCard(){
-        if(handcards.size() > 0){ //Wenn die AI Handkarten hat
-            if(fieldcards.size() == 0){ //Wenn die AI keine Feldkarten hat, soll bevorzugt werden, niedere Karten zu beschwören --> für Tribute
-                summonWeakCard();
-            }else{ //Soll erst überprüft werden, ob eine Tributbeschwörung durchgeführt werden kann
-                int index = 0;
-                boolean summon = false;
-                int valuable = 0;
+        summonCardByTribute(GameSettings.maxTributes + 1); //Beschwört die beste Karte von der Anzahl an Tributen (rekursiv)
+    }
 
-                for(Card card : fieldcards){ //Karten, die mindestens ein Tribut benötigten werden als "wertvoll" eingestuft und sollen nicht so einfach geopfert werden
-                    if(card.getTribute().getNeededCards() > 0){
-                        valuable += 1;
-                    }
+
+    private void summonCardByTribute(int tributeValue){
+        if(tributeValue == 0){ //Wenn keine Tribute gefordert sind
+            for(int i = 0; i < handcards.size(); i++){
+                if(handcards.get(i).getTribute().getNeededCards() == tributeValue){
+                    summonDirectly(i);
+                    break;
                 }
+            }
+        }else if(tributeValue > 0){ //Wenn mindestens ein Tribut benötigt wird
+            ArrayList<Integer> valueSums = new ArrayList<>(); //ATK und DEF werden für jede Karte addiert und für den jeweiligen Index temporär eingespeichert
+            boolean summon = false;
+            int summonIndex = 0;
+            for(int i = 0; i < handcards.size(); i++){
+                if(handcards.get(i).getTribute().getNeededCards() == tributeValue){ //Sucht Karten in der Hand nach der gewünschten Anzahl der geforderten Tributen
+                    if(fieldcards.size() >= tributeValue){ //Wenn eine Tributbeschwörung möglich wäre
+                        int counter = 0; // Zählt die Karten mit einem schwächeren Tribut, als das zu beschworene (tributValue - 1)
+                        for(int j = 0; j < fieldcards.size(); j++){ //Überprüft, ob sich die Tributbeschwörung denn lohnen würde
+                            int sum = fieldcards.get(j).getDamage() + fieldcards.get(j).getLife();
 
-                int tributeSummon = fieldcards.size() - valuable; //So viele Karten können geopfert werden
-                for(int i = 0; i < handcards.size(); i++){ //findet den Index für die beste Tributbeschwörung
-                    if(handcards.get(i).getTribute().getNeededCards() == tributeSummon){
-                        index = i;
-                        summon = true;
-                    }
-                }
-
-                if(summon && handcards.get(index).getTribute().getNeededCards() == 0){ //Normale Beschwörung
-                    summonDirectly(index);
-                }else if(summon && handcards.get(index).getTribute().getNeededCards() > 0){ //Tributbeschwörung
-                    ArrayList<Integer> destroyingCards = new ArrayList<>();
-                    for(int i = 0; i < fieldcards.size(); i++){
-                        if(fieldcards.get(i).getTribute().getNeededCards() < 0) destroyingCards.add(i);
-                        if(destroyingCards.size() == handcards.get(index).getTribute().getNeededCards()) break;
-                    }
-
-                    if(destroyingCards.size() == handcards.get(index).getTribute().getNeededCards()){
-                        for(int i : destroyingCards){
-                            fieldcards.get(i).destroy();
+                            if(fieldcards.get(j).getTribute().getNeededCards() < tributeValue){
+                                counter += 1;
+                            }
+                            valueSums.add(sum);
                         }
-                        summonDirectly(index);
-                    }else{
-                        if(fieldcards.size() == handcards.get(index).getTribute().getNeededCards()){
-                            for(int i = 0; i < fieldcards.size(); i++){
-                                destroyingCards.add(i);
-                            }
 
-                            for(int i : destroyingCards){
-                                fieldcards.get(i).destroy();
-                            }
-                            summonDirectly(index);
+                        if(counter >= handcards.get(i).getTribute().getNeededCards()){
+                            summon = true;
+                            summonIndex = i;
+                            break;
                         }
                     }
                 }
             }
-        }
-    }
 
-    private void summonWeakCard(){
-        ArrayList<Integer> tributes = new ArrayList<>(); //Ein Array für die Anzahl der Tribute jeder Handkarte. Beispiel = {0, 0, 0, 1, 2}
-        for(Card handcard : handcards){ //Für jede Handkarte überprüfen, ob sie Tribute braucht (wenn ja, wie viele)
-            tributes.add(handcard.getTribute().getNeededCards());
-        }
-
-        if(fieldcards.size() == 0) { //Wenn die AI keine Feldkarten hat, soll bevorzugt werden, niedere Karten zu beschwören --> für Tribute
-            for (int i = 0; i < tributes.size(); i++) {
-                if (tributes.get(i) == 0) { //Wenn eine Karte gefunden wurde (auf der Hand), die keine Tribute braucht
-                    summonDirectly(i); //Sie wird beschworen
+            if(summon){ //Die niedrigsten Werte werden geopfert
+                ArrayList<Integer> indicies = new ArrayList<>();
+                for(int i = 0; i < handcards.get(summonIndex).getTribute().getNeededCards(); i++){ //Wird z.B. 2 Mal ausgeführt (weil 2 Karten geopfert werden)
+                    int currentIndex = 0;
+                    int currentLowest = 0;
+                    for(int j = 0; j < valueSums.size(); j++){ //Summen vergleichen
+                        if(currentLowest == 0){ //Erste Summe wird in currentLowest initialisiert
+                            currentLowest = valueSums.get(j);
+                        }
+                        if(currentLowest > valueSums.get(j)){
+                            currentIndex = j;
+                        }
+                    }
+                    indicies.add(currentIndex);
+                    valueSums.remove(currentIndex);
                 }
-            }
+
+                Gdx.app.debug("BUG", "indicies: " + indicies.toString() + ", Beschwörung: " + handcards.get(summonIndex).getName());
+                for(int i : indicies){
+                    fieldcards.get(i).destroy();
+                }
+
+                summonDirectly(summonIndex);
+
+            }else if(!summon && tributeValue >= 0){summonCardByTribute(tributeValue - 1); }//Rekursionsverankerung --> tributeValue < 0 ist nicht gültig
         }
     }
 
+    /**
+     * @author Noah O. Wantoch
+     * Activates the effect, if the situation is good
+     * */
     private void checkEffect(Effect effect){
         if(effect.getEffectModule() == EffectModule.DESTROY_N){ //Dieser Effekt soll nur auf den Spieler gehen
             if(CardGameInstances.playerFieldcards.size() > 0){ //Wenn der Spieler Feldkarten hat
